a74ceeaccb7a0a05b2e5d2a0d8008174b8e40cb2
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-02 04:44:05 +0800

    用create的方法新增資料

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post = new Post();
-    $post -> title = 'test title';
-    $post -> content = 'test content';
-    $post -> save();
+    //$post = new Post();
+    //$post -> title = 'test title';
+    //$post -> content = 'test content';
+    //$post -> save();
+
+    Post::create([
+        'title' =>
+        'content' =>
+    ])
 });
